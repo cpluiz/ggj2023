4469915286a9603e248959a5592af151f3ba1d2b
Luiz Fernando de Lira <luiz.lira@fatec.sp.gov.br>
2023-02-04 20:26:10 -0300

    adicionado mapa, ajustado o tamanho, e implementada a câmera que segue a personagem

---
+++ Assets/scripts/TopDownCAM.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCAM : MonoBehaviour
+{
+
+    public float velocidade = 3.0f;
+    public Transform target;
+    Vector3 mousePos;
+    public Vector3 offset = Vector3.up;
+    public Camera cam;
+    public bool LookAt = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, velocidade*Time.deltaTime);
+
+        if (LookAt)
+        { 
+            transform.LookAt(target.position);
+        }
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    
+}
